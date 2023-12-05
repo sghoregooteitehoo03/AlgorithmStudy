6fae54eb152d61f87ff0b25203b96e0a20b4e197
sghore <sghoregooteitehoo03@gmail.com>
2023-12-05 16:42:51 +0900

    5_6 답안 최적화

--- DFS/BFS/5_6.py
+++ DFS/BFS/5_6.py
@@ -2,7 +2,6 @@ from collections import deque
 
 def bfs(graph, start, visited, movements):
     visited[0][0] = True
-    result = [[1] * m for _ in range(n)]
     queue = deque([start])
 
     while queue:
@@ -11,7 +10,7 @@ def bfs(graph, start, visited, movements):
         dx = currentPos[1]
 
         if dy == len(graph) - 1 and dx == len(graph[0]) - 1:
-            print(result[dy][dx])
+            print(graph[dy][dx])
             break
 
         for movement in movements:
@@ -24,11 +23,11 @@ def bfs(graph, start, visited, movements):
             elif (nextDy > len(graph) - 1 or nextDx > len(graph[0]) - 1):
                 continue
 
-            if graph[nextDy][nextDx] == '1' and not visited[nextDy][nextDx]:
+            if graph[nextDy][nextDx] == 1 and not visited[nextDy][nextDx]:
                 queue.append([nextDy, nextDx])
                 visited[nextDy][nextDx] = True
                 
-                result[nextDy][nextDx] = result[dy][dx] + 1
+                graph[nextDy][nextDx] = graph[dy][dx] + 1
     
 
 n, m = map(int, input().split())
@@ -38,6 +37,6 @@ movements = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 for i in range(n):
     inputData = input()
-    room.append(inputData)
+    room.append(list(map(int, inputData)))
 
 bfs(room, [0, 0], visited, movements)
