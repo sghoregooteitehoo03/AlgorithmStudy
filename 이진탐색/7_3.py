d2103d1defe725d208a43d559392cb4faef08ca3
sghore <sghoregooteitehoo03@gmail.com>
2023-12-07 16:18:15 +0900

    이진탐색 알고리즘 정리

---
+++ 이진탐색/7_3.py
@@ -0,0 +1,19 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if target == array[mid]:  # 찾은 경우
+            return mid
+        elif target < array[mid]:  # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+            end = mid - 1
+        else:  # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+            start = mid + 1
+    return None
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
