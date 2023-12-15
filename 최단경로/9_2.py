e020e03f1950a67d7dd61a663ad7aee4e38f4cf8
sghore <sghoregooteitehoo03@gmail.com>
2023-12-15 15:14:55 +0900

    최단경로 문제풀이

--- 최단경로/9_2.py
+++ 최단경로/9_2.py
@@ -9,8 +9,6 @@ n, m = map(int, input().split())
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
 graph = [[] for i in range(n + 1)]
-# 방문한 적이 있는지 확인하는 리스트
-visited = [False] * (n + 1)
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n + 1)
 
@@ -37,7 +35,7 @@ def dijkstra(start):
         for i in graph[now]:
             cost = dist + i[1]
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[0]:
+            if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
