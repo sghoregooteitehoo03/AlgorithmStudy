ecde8b28010ce6eac5f660ec8053ef0e5a49dc43
sghore <sghoregooteitehoo03@gmail.com>
2024-02-26 19:42:41 +0900

    DFS/BFS 기출문제 재풀이(Q_21 O)

--- DFS/BFS/기출문제/Q_21.py
+++ DFS/BFS/기출문제/Q_21.py
@@ -1,10 +1,21 @@
 # https://www.acmicpc.net/problem/16234
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-def close_country(map_graph):
-    for i in range(len(map_graph)):
-        for j in range(len(map_graph)):
-            map_graph[i][j] = (map_graph[i][j], (i * len(map_graph)) + j)
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 n, l, r = map(int, input().split())
 map_graph = []
@@ -14,82 +25,62 @@ for i in range(n):
 
 is_moved = True
 result = -1
+parent = [0] * (n * n)
 
 while is_moved:
-    close_country(map_graph)
     result += 1
 
     is_moved = False
+    cal_dict = {}
     visited = [[False] * n for _ in range(n)]
-    dict = {}
+    visited[0][0] = True
+
+    for i in range( n * n):
+        parent[i] = i
 
     q = deque([(0, 0)])
-
     while q:
         pos_i, pos_j = q.popleft()
-
-        if not visited[pos_i][pos_j]:
-            visited[pos_i][pos_j] = True
-
-            if pos_i - 1 >= 0:
-                q.append((pos_i - 1, pos_j))
-
-                diff = abs(map_graph[pos_i][pos_j][0] - map_graph[pos_i - 1][pos_j][0])
-                if diff >= l and diff <= r:
-                    if map_graph[pos_i][pos_j][1] < map_graph[pos_i - 1][pos_j][1]:
-                        map_graph[pos_i - 1][pos_j] = (map_graph[pos_i - 1][pos_j][0], map_graph[pos_i][pos_j][1])
-                    else:
-                        map_graph[pos_i][pos_j] = (map_graph[pos_i][pos_j][0], map_graph[pos_i - 1][pos_j][1])
-
-            if pos_i + 1 < n:
+        
+        if pos_i + 1 < n:
+            if not visited[pos_i  + 1][pos_j]:
+                visited[pos_i + 1][pos_j] = True
                 q.append((pos_i + 1, pos_j))
+            diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i + 1][pos_j])
 
-                diff = abs(map_graph[pos_i][pos_j][0] - map_graph[pos_i + 1][pos_j][0])
-                if diff >= l and diff <= r:
-                    if map_graph[pos_i][pos_j][1] < map_graph[pos_i + 1][pos_j][1]:
-                        map_graph[pos_i + 1][pos_j] = (map_graph[pos_i + 1][pos_j][0], map_graph[pos_i][pos_j][1])
-                    else:
-                        map_graph[pos_i][pos_j] = (map_graph[pos_i][pos_j][0], map_graph[pos_i + 1][pos_j][1])
+            if diff >= l and diff <= r:
+                union_parent(parent, (pos_i * n) + pos_j, ((pos_i + 1) * n) + pos_j)
 
-            if pos_j - 1 >= 0:
-                q.append((pos_i, pos_j - 1))
-
-                diff = abs(map_graph[pos_i][pos_j][0] - map_graph[pos_i][pos_j - 1][0])
-                if diff >= l and diff <= r:
-                    if map_graph[pos_i][pos_j][1] < map_graph[pos_i][pos_j - 1][1]:
-                        map_graph[pos_i][pos_j - 1] = (map_graph[pos_i][pos_j - 1][0], map_graph[pos_i][pos_j][1])
-                    else:
-                        map_graph[pos_i][pos_j] = (map_graph[pos_i][pos_j][0], map_graph[pos_i][pos_j - 1][1])
-
-            if pos_j + 1 < n:
+        if pos_j + 1 < n:
+            if not visited[pos_i][pos_j + 1]:
+                visited[pos_i][pos_j + 1] = True
                 q.append((pos_i, pos_j + 1))
+            diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i][pos_j + 1])
 
-                diff = abs(map_graph[pos_i][pos_j][0] - map_graph[pos_i][pos_j + 1][0])
-                if diff >= l and diff <= r:
-                    if map_graph[pos_i][pos_j][1] < map_graph[pos_i][pos_j + 1][1]:
-                        map_graph[pos_i][pos_j + 1] = (map_graph[pos_i][pos_j + 1][0], map_graph[pos_i][pos_j][1])
-                    else:
-                        map_graph[pos_i][pos_j] = (map_graph[pos_i][pos_j][0], map_graph[pos_i][pos_j + 1][1])
+            if diff >= l and diff <= r:
+                union_parent(parent, (pos_i * n) + pos_j, (pos_i * n) + (pos_j + 1))
     
-    for i in range(n):
-        for j in range(n):
-            if dict.get(map_graph[i][j][1]) == None:
-                dict[map_graph[i][j][1]] = [(map_graph[i][j][0], i, j)]
-            else:
-                dict[map_graph[i][j][1]].append((map_graph[i][j][0], i, j))
+    for i in range(n * n):
+        index = find_parent(parent, parent[i])
+        pos_i = i // n
+        pos_j = i % n
+        
+        if cal_dict.get(index) == None:
+            cal_dict[index] = [(map_graph[pos_i][pos_j], pos_i, pos_j)]
+        else:
+            cal_dict[index].append((map_graph[pos_i][pos_j], pos_i, pos_j))
 
-    for key in dict.keys():
-        if len(dict[key]) == 1:
-            map_graph[dict[key][0][1]][dict[key][0][2]] = map_graph[dict[key][0][1]][dict[key][0][2]][0]
+    for key in cal_dict.keys():
+        if len(cal_dict[key]) == 1:
             continue
 
         is_moved = True
         summary = 0
-        for data in dict[key]:
+        for data in cal_dict[key]:
             summary += data[0]
 
-        summary = summary // len(dict[key])
-        for data in dict[key]:
+        summary = summary // len(cal_dict[key])
+        for data in cal_dict[key]:
             map_graph[data[1]][data[2]] = summary
 
 print(result)
