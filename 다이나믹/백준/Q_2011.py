5ce7b0a9a631b444d28753ea85f58ffd5a62b66d
sghore <sghoregooteitehoo03@gmail.com>
2024-05-13 23:35:42 +0900

    dynamic (Q_2011 O, Q_2240 푸는 중)

--- 다이나믹/백준/Q_2011.py
+++ 다이나믹/백준/Q_2011.py
@@ -2,3 +2,24 @@ import sys
 input = sys.stdin.readline
 
 code = input().rstrip()
+dp = [0] * len(code)
+
+for i in range(len(code) - 1, -1, -1):
+    if i == len(code) - 1:
+        if int(code[i]) > 0:
+            dp[i] = 1
+    else:
+        if int(code[i]) > 0:
+            dp[i] = dp[i + 1]
+
+            cal = int(code[i] + code[i + 1])
+            if cal < 27 and cal >=10:
+                if i + 2 < len(code):
+                    dp[i] += dp[i + 2]
+                else:
+                    dp[i] += 1
+
+if dp[0] == 0:
+    print(0)
+else:
+    print(dp[0] % 1000000)
---
+++ 다이나믹/백준/Q_2240.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+t, w = map(int, input().split())
+arr = []
+
+for i in range(t):
+    arr.append(int(input()))
