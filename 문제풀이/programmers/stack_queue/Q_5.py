e11df80e24d73076ffd6a8050a007c4db9d082aa
sghore <sghoregooteitehoo03@gmail.com>
2024-08-30 13:34:10 +0900

    stack (Q_5 푸는 중)

--- 문제풀이/programmers/stack_queue/Q_5.py
+++ 문제풀이/programmers/stack_queue/Q_5.py
@@ -1,19 +1,33 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     stack = []
+    total = 0
+    i = 0
 
-    for truck_weight in truck_weights:
-        stack.append(truck_weight)
+    while i < len(truck_weights):
+        truck_weight = truck_weights[i]
+        total += truck_weight
+        
+        if len(stack) == 0 or stack[0][0] == 1:
+            stack.append((bridge_length, truck_weight))
+        else:
+            stack.append((1, truck_weight))
 
-        if sum(stack) > weight:
+        if total > weight:
+            total -= truck_weight
             stack.pop(-1)
-            answer += len(stack) * bridge_length
 
-            stack.clear()
-            stack.append(truck_weight)
+            v, w = stack.pop(0)
+            answer += v
+            total -= w
+            continue
+        
+        i += 1
 
-    answer += len(stack)
+    print(stack)
+    if len(stack
 
-    return answer
+    return answer + 1
 
-print(solution(2, 10, [7,4,5,6]))
+# print(solution(2, 10, [7,4,5,6]))
+print(solution(5, 5, [2, 2, 2, 2, 1, 1, 1, 1, 1]))
