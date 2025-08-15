287dc571f92c1b5192956157378a7606f9af3745
sghore <sghoregooteitehoo03@gmail.com>
2025-08-15 15:24:11 +0900

    소수판별 문제, 조합 문제 실전기출문제 풀이

--- 기타/Test1.py
+++ 기타/Test1.py
@@ -1,20 +1,37 @@
 import math
-
 m, n = map(int, input().split())
-array = [True] * (1000001)
-array[1] = False
 
-for i in range(2, int(math.sqrt(n)) + 1):
+def is_prime_number(number):
+    if number == 1:
+        return False
     
-    if array[i] == True: #i가 소수인 경우(남은 수인 경우)
-        j = 2
-        while i * j <= n:
-            array[i * j] = False
-            j += 1
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    
+    return True
 
 for i in range(m, n + 1):
-    if i == 1:
-        continue
-
-    if array[i] == True:
+    if is_prime_number(i):
         print(i)
+
+# import math
+
+# m, n = map(int, input().split())
+# array = [True] * (1000001)
+# array[1] = False
+
+# for i in range(2, int(math.sqrt(n)) + 1):
+    
+#     if array[i] == True: #i가 소수인 경우(남은 수인 경우)
+#         j = 2
+#         while i * j <= n:
+#             array[i * j] = False
+#             j += 1
+
+# for i in range(m, n + 1):
+#     if i == 1:
+#         continue
+
+#     if array[i] == True:
+#         print(i)
--- 기타/Test2.py
+++ 기타/Test2.py
@@ -1,20 +1,18 @@
-from itertools import combinations
+import itertools
 
 l, c = map(int, input().split())
-ap_list = list(input().split())
-ap_list.sort()
+arr = list(input().split())
+arr.sort()
+condition = ['a', 'e', 'i', 'o', 'u']
 
-combine = combinations(ap_list, l)
-result = 0
-for data in combine:
-    consonants = 0 # 자음
-    vowels = 0 # 모음
-    
-    for C in data:
-        if C == 'a' or C == 'e' or C == 'i' or C == 'o' or C == 'u':
-            vowels += 1
+for x in itertools.combinations(arr, l):
+    count1 = 0
+    count2 = 0
+    for password in list(x):
+        if password in condition:
+            count1 += 1
         else:
-            consonants += 1
-    
-    if vowels >= 1 and consonants >= 2:
-        print(''.join(map(str, data)))
+            count2 += 1
+            
+    if count1 >= 1 and count2 >= 2:
+        print(''.join(list(x)))
