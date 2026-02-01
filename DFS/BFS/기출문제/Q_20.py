97d8eb42ff383a419f715cb5a2ec1314f8ca5344
sghore <sghoregooteitehoo03@gmail.com>
2026-02-01 13:36:41 +0900

    DFS/BFS(Q_20 풀이)

--- DFS/BFS/기출문제/Q_20.py
+++ DFS/BFS/기출문제/Q_20.py
@@ -1,92 +1,169 @@
 # https://www.acmicpc.net/problem/18428
 from collections import deque
-import copy
-import sys
-input = sys.stdin.readline
-
-def check_student(map_table, start_pos):
-    q = deque(start_pos)
-    is_find = False
-
-    while(q):
-        previous_i, previous_j, pos_i, pos_j = q.popleft()
-
-        if map_table[pos_i][pos_j] == 'O':
-            continue
-
-        if map_table[pos_i][pos_j] == 'S':
-            is_find = True
-            break
-
-        if pos_i - 1 >= 0 and (previous_i == pos_i + 1 or previous_i == 0):
-            q.append((pos_i, -2, pos_i - 1, pos_j))
-        if pos_i + 1 < len(map_table) and (previous_i == pos_i - 1 or previous_i == 0):
-            q.append((pos_i, -2, pos_i + 1, pos_j))
-        if pos_j - 1 >= 0 and (previous_j == pos_j + 1 or previous_j == 0):
-            q.append((-2, pos_j, pos_i, pos_j - 1))
-        if pos_j + 1 < len(map_table[0]) and (previous_j == pos_j - 1 or previous_j == 0):
-            q.append((-2, pos_j, pos_i, pos_j + 1))
-
-    return is_find
-
-def flatten_2d_array(arr_2d):
-    return [element for sublist in arr_2d for element in sublist]
-
-def reshape_to_2d_array(arr_1d, rows, cols):
-    return [arr_1d[i:i+cols] for i in range(0, len(arr_1d), cols)]
+from copy import deepcopy
 
 n = int(input())
-map_table = []
-start_pos = []
+board = []
 
 for i in range(n):
-    map_table.append(list(input().split()))
+    board.append(input().split())
 
+t_pos = []
+empty_pos = []
 for i in range(n):
     for j in range(n):
-        if map_table[i][j] == 'T':
-            start_pos.append((0, 0, i, j))
+        if board[i][j] == "X":
+            empty_pos.append((i, j))
+        elif board[i][j] == "T":
+            t_pos.append((i, j, "x"))
 
-flat_map = flatten_2d_array(map_table)
-is_find = True
 
-for i in range(len(flat_map) - 2):
-    copy_map = copy.deepcopy(flat_map)
+def spread_teacher(board):
+    queue = deque(t_pos)
 
-    if copy_map[i] == 'X':
-        copy_map[i] = 'O'
-    else:
-        continue
+    while queue:
+        i, j, movement = queue.popleft()
 
-    for j in range(i + 1, len(flat_map) - 1):
-        sub_map1 = copy.deepcopy(copy_map)
-
-        if copy_map[j] == 'X':
-            copy_map[j] = 'O'
-        else:
+        if i < 0 or i >= n or j < 0 or j >= n:
             continue
 
-        for k in range(j + 1, len(flat_map)):
-            sub_map2 = copy.deepcopy(copy_map)
-            if copy_map[k] == 'X':
-                copy_map[k] = 'O'
-            else:
-                continue
+        if board[i][j] == "S":
+            return False
 
-            is_find = check_student(reshape_to_2d_array(copy_map, n, n), start_pos)    
-            if not is_find:
-                break
-            
-            copy_map = sub_map2
-        
-        if not is_find:
-            break
-        copy_map = sub_map1
-    
-    if not is_find:
-        break
+        if movement != "x" and board[i][j] == "O":
+            continue
 
-if is_find:
-    print("NO")
-else:
+        board[i][j] = "T"
+        if movement == "u":
+            queue.append((i + 1, j, movement))
+        elif movement == "d":
+            queue.append((i - 1, j, movement))
+        elif movement == "l":
+            queue.append((i, j - 1, movement))
+        elif movement == "r":
+            queue.append((i, j + 1, movement))
+        else:
+            queue.append((i + 1, j, "u"))
+            queue.append((i - 1, j, "d"))
+            queue.append((i, j - 1, "l"))
+            queue.append((i, j + 1, "r"))
+
+    return True
+
+
+def set_wall():
+    for i in range(len(empty_pos)):
+        copy_board = deepcopy(board)
+        copy_board[empty_pos[i][0]][empty_pos[i][1]] = "O"
+
+        for j in range(i + 1, len(empty_pos)):
+            copy_board[empty_pos[j][0]][empty_pos[j][1]] = "O"
+
+            for k in range(j + 1, len(empty_pos)):
+                copy_board[empty_pos[k][0]][empty_pos[k][1]] = "O"
+
+                if spread_teacher(deepcopy(copy_board)):
+                    return True
+                copy_board[empty_pos[k][0]][empty_pos[k][1]] = "X"
+            copy_board[empty_pos[j][0]][empty_pos[j][1]] = "X"
+        copy_board[empty_pos[i][0]][empty_pos[i][1]] = "X"
+
+    return False
+
+
+if set_wall():
     print("YES")
+else:
+    print("NO")
+
+# from collections import deque
+# import copy
+# import sys
+# input = sys.stdin.readline
+
+# def check_student(map_table, start_pos):
+#     q = deque(start_pos)
+#     is_find = False
+
+#     while(q):
+#         previous_i, previous_j, pos_i, pos_j = q.popleft()
+
+#         if map_table[pos_i][pos_j] == 'O':
+#             continue
+
+#         if map_table[pos_i][pos_j] == 'S':
+#             is_find = True
+#             break
+
+#         if pos_i - 1 >= 0 and (previous_i == pos_i + 1 or previous_i == 0):
+#             q.append((pos_i, -2, pos_i - 1, pos_j))
+#         if pos_i + 1 < len(map_table) and (previous_i == pos_i - 1 or previous_i == 0):
+#             q.append((pos_i, -2, pos_i + 1, pos_j))
+#         if pos_j - 1 >= 0 and (previous_j == pos_j + 1 or previous_j == 0):
+#             q.append((-2, pos_j, pos_i, pos_j - 1))
+#         if pos_j + 1 < len(map_table[0]) and (previous_j == pos_j - 1 or previous_j == 0):
+#             q.append((-2, pos_j, pos_i, pos_j + 1))
+
+#     return is_find
+
+# def flatten_2d_array(arr_2d):
+#     return [element for sublist in arr_2d for element in sublist]
+
+# def reshape_to_2d_array(arr_1d, rows, cols):
+#     return [arr_1d[i:i+cols] for i in range(0, len(arr_1d), cols)]
+
+# n = int(input())
+# map_table = []
+# start_pos = []
+
+# for i in range(n):
+#     map_table.append(list(input().split()))
+
+# for i in range(n):
+#     for j in range(n):
+#         if map_table[i][j] == 'T':
+#             start_pos.append((0, 0, i, j))
+
+# flat_map = flatten_2d_array(map_table)
+# is_find = True
+
+# for i in range(len(flat_map) - 2):
+#     copy_map = copy.deepcopy(flat_map)
+
+#     if copy_map[i] == 'X':
+#         copy_map[i] = 'O'
+#     else:
+#         continue
+
+#     for j in range(i + 1, len(flat_map) - 1):
+#         sub_map1 = copy.deepcopy(copy_map)
+
+#         if copy_map[j] == 'X':
+#             copy_map[j] = 'O'
+#         else:
+#             continue
+
+#         for k in range(j + 1, len(flat_map)):
+#             sub_map2 = copy.deepcopy(copy_map)
+#             if copy_map[k] == 'X':
+#                 copy_map[k] = 'O'
+#             else:
+#                 continue
+
+#             is_find = check_student(reshape_to_2d_array(copy_map, n, n), start_pos)
+#             if not is_find:
+#                 break
+
+#             copy_map = sub_map2
+
+#         if not is_find:
+#             break
+#         copy_map = sub_map1
+
+#     if not is_find:
+#         break
+
+# if is_find:
+#     print("NO")
+# else:
+#     print("YES")
--- DFS/BFS/기출문제/Q_21.py
+++ DFS/BFS/기출문제/Q_21.py
@@ -1,86 +1,144 @@
 # https://www.acmicpc.net/problem/16234
 from collections import deque
-import sys
-input = sys.stdin.readline
-
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
 
 n, l, r = map(int, input().split())
-map_graph = []
+board = []
 
 for i in range(n):
-    map_graph.append(list(map(int, input().split())))
+    board.append(list(map(int, input().split())))
 
-is_moved = True
-result = -1
-parent = [0] * (n * n)
+answer = 0
 
-while is_moved:
-    result += 1
+while True:
+    group = [[] for _ in range(n * n)]
+    group[0].append((board[0][0], 0, 0))
+    queue = deque([(0, 0, board[0][0], 0)])
+    grouped = [[False] * n for _ in range(n)]
 
-    is_moved = False
-    cal_dict = {}
-    visited = [[False] * n for _ in range(n)]
-    visited[0][0] = True
+    is_start = True
 
-    for i in range( n * n):
-        parent[i] = i
+    while queue:
+        i, j, previous_value, previous_group_id = queue.popleft()
 
-    q = deque([(0, 0)])
-    while q:
-        pos_i, pos_j = q.popleft()
-        
-        if pos_i + 1 < n:
-            if not visited[pos_i  + 1][pos_j]:
-                visited[pos_i + 1][pos_j] = True
-                q.append((pos_i + 1, pos_j))
-            diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i + 1][pos_j])
-
-            if diff >= l and diff <= r:
-                union_parent(parent, (pos_i * n) + pos_j, ((pos_i + 1) * n) + pos_j)
-
-        if pos_j + 1 < n:
-            if not visited[pos_i][pos_j + 1]:
-                visited[pos_i][pos_j + 1] = True
-                q.append((pos_i, pos_j + 1))
-            diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i][pos_j + 1])
-
-            if diff >= l and diff <= r:
-                union_parent(parent, (pos_i * n) + pos_j, (pos_i * n) + (pos_j + 1))
-    
-    for i in range(n * n):
-        index = find_parent(parent, parent[i])
-        pos_i = i // n
-        pos_j = i % n
-        
-        if cal_dict.get(index) == None:
-            cal_dict[index] = [(map_graph[pos_i][pos_j], pos_i, pos_j)]
-        else:
-            cal_dict[index].append((map_graph[pos_i][pos_j], pos_i, pos_j))
-
-    for key in cal_dict.keys():
-        if len(cal_dict[key]) == 1:
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board):
             continue
 
-        is_moved = True
-        summary = 0
-        for data in cal_dict[key]:
-            summary += data[0]
+        current_value = board[i][j]
 
-        summary = summary // len(cal_dict[key])
-        for data in cal_dict[key]:
-            map_graph[data[1]][data[2]] = summary
+        if is_start or (
+            abs(current_value - previous_value) >= l
+            and abs(current_value - previous_value) <= r
+        ):
+            if not is_start and not grouped[i][j]:
+                group[previous_group_id].append((current_value, i, j))
+                grouped[i][j] = True
 
-print(result)
+            queue.append((i + 1, j, current_value, previous_group_id))
+            queue.append((i, j + 1, current_value, previous_group_id))
+        else:
+            queue.append((i + 1, j, current_value, previous_group_id + 1))
+            queue.append((i, j + 1, current_value, previous_group_id + 1))
+
+        if is_start:
+            is_start = False
+
+    if len(group[0]) == 1:
+        break
+
+    for g in group:
+        if len(g) == 0:
+            break
+        sum_value = sum(t[0] for t in g)
+        result = sum_value // len(g)
+
+        for content in g:
+            board[content[1]][content[2]] = result
+
+    answer += 1
+print(answer)
+
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent, parent[x])
+#     return parent[x]
+
+# def union_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+
+# n, l, r = map(int, input().split())
+# map_graph = []
+
+# for i in range(n):
+#     map_graph.append(list(map(int, input().split())))
+
+# is_moved = True
+# result = -1
+# parent = [0] * (n * n)
+
+# while is_moved:
+#     result += 1
+
+#     is_moved = False
+#     cal_dict = {}
+#     visited = [[False] * n for _ in range(n)]
+#     visited[0][0] = True
+
+#     for i in range( n * n):
+#         parent[i] = i
+
+#     q = deque([(0, 0)])
+#     while q:
+#         pos_i, pos_j = q.popleft()
+
+#         if pos_i + 1 < n:
+#             if not visited[pos_i  + 1][pos_j]:
+#                 visited[pos_i + 1][pos_j] = True
+#                 q.append((pos_i + 1, pos_j))
+#             diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i + 1][pos_j])
+
+#             if diff >= l and diff <= r:
+#                 union_parent(parent, (pos_i * n) + pos_j, ((pos_i + 1) * n) + pos_j)
+
+#         if pos_j + 1 < n:
+#             if not visited[pos_i][pos_j + 1]:
+#                 visited[pos_i][pos_j + 1] = True
+#                 q.append((pos_i, pos_j + 1))
+#             diff = abs(map_graph[pos_i][pos_j] - map_graph[pos_i][pos_j + 1])
+
+#             if diff >= l and diff <= r:
+#                 union_parent(parent, (pos_i * n) + pos_j, (pos_i * n) + (pos_j + 1))
+
+#     for i in range(n * n):
+#         index = find_parent(parent, parent[i])
+#         pos_i = i // n
+#         pos_j = i % n
+
+#         if cal_dict.get(index) == None:
+#             cal_dict[index] = [(map_graph[pos_i][pos_j], pos_i, pos_j)]
+#         else:
+#             cal_dict[index].append((map_graph[pos_i][pos_j], pos_i, pos_j))
+
+#     for key in cal_dict.keys():
+#         if len(cal_dict[key]) == 1:
+#             continue
+
+#         is_moved = True
+#         summary = 0
+#         for data in cal_dict[key]:
+#             summary += data[0]
+
+#         summary = summary // len(cal_dict[key])
+#         for data in cal_dict[key]:
+#             map_graph[data[1]][data[2]] = summary
+
+# print(result)
