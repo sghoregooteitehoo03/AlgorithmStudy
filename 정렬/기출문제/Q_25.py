5e547ca947671d0249d74102bda92998ccb489c1
sghore <sghoregooteitehoo03@gmail.com>
2026-02-10 19:21:17 +0900

    정렬 (Q_25 ~ 26 풀이)

--- 정렬/기출문제/Q_25.py
+++ 정렬/기출문제/Q_25.py
@@ -1,32 +1,51 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42889
 def solution(N, stages):
     answer = []
-    stage_percent = [0] * (N + 2)
-    previous = 0
+    counts = [0] * 502
 
     for stage in stages:
-        stage_percent[stage] += 1
-    
-    for i in range(len(stage_percent)):
-        if i > N:
-            continue
-        elif stage_percent[i] == 0:
-            if i > 0 and i <= N:
-                answer.append((0, i))
-            continue
-        
-        fail_percent = stage_percent[i] / (len(stages) - previous)
-        print(fail_percent)
-        answer.append((fail_percent, i))
+        counts[stage] += 1
 
-        previous += stage_percent[i]
+    person = len(stages)
+    for i in range(1, N + 1):
+        if person <= 0:
+            answer.append((0.0, i))
+            continue
+
+        rate = counts[i] / person
+        answer.append((rate, i))
+
+        person -= counts[i]
 
     answer.sort(key=lambda x: (-x[0], x[1]))
-    for i in range(len(answer)):
-        answer[i] = answer[i][1]
+    result = [x[1] for x in answer]
+    return result
 
-    return answer
 
-# solution(5, [2, 1, 2, 6, 2, 4, 3, 3])
-# solution(4, [4,4,4,4,4])
-solution(5, [1, 1, 1, 2, 3, 4])
+# def solution(N, stages):
+#     answer = []
+#     stage_percent = [0] * (N + 2)
+#     previous = 0
+
+#     for stage in stages:
+#         stage_percent[stage] += 1
+
+#     for i in range(len(stage_percent)):
+#         if i > N:
+#             continue
+#         elif stage_percent[i] == 0:
+#             if i > 0 and i <= N:
+#                 answer.append((0, i))
+#             continue
+
+#         fail_percent = stage_percent[i] / (len(stages) - previous)
+#         print(fail_percent)
+#         answer.append((fail_percent, i))
+
+#         previous += stage_percent[i]
+
+#     answer.sort(key=lambda x: (-x[0], x[1]))
+#     for i in range(len(answer)):
+#         answer[i] = answer[i][1]
+
+#     return answer
--- 정렬/기출문제/Q_26.py
+++ 정렬/기출문제/Q_26.py
@@ -2,26 +2,46 @@
 import heapq
 
 n = int(input())
-array = []
-result = 0
+queue = []
 
 for i in range(n):
-    array.append(int(input()))
+    heapq.heappush(queue, int(input()))
 
-if n == 1:
-    print(0)
-else:
-    q = []
-    for a in array:
-        heapq.heappush(q, a)
+result = 0
+while queue:
+    q1 = heapq.heappop(queue)
 
-    while(q):
-        val1 = heapq.heappop(q)
-        
-        if len(q) != 0:
-            val2 = heapq.heappop(q)
-            
-            result += (val1 + val2)
-            heapq.heappush(q, val1 + val2)
+    if len(queue) >= 1:
+        q2 = heapq.heappop(queue)
+        result += q1 + q2
 
-    print(result)
+        heapq.heappush(queue, q1 + q2)
+
+print(result)
+
+# import heapq
+
+# n = int(input())
+# array = []
+# result = 0
+
+# for i in range(n):
+#     array.append(int(input()))
+
+# if n == 1:
+#     print(0)
+# else:
+#     q = []
+#     for a in array:
+#         heapq.heappush(q, a)
+
+#     while(q):
+#         val1 = heapq.heappop(q)
+
+#         if len(q) != 0:
+#             val2 = heapq.heappop(q)
+
+#             result += (val1 + val2)
+#             heapq.heappush(q, val1 + val2)
+
+#     print(result)
