a2dfdaa4083d5e761d46df06bf550158ec84261c
sghore <sghoregooteitehoo03@gmail.com>
2026-02-21 13:49:18 +0900

    다이나믹(Q_35~36 재풀이) 최단경로(Q_38~39 재풀이)

--- 다이나믹/기출문제/Q_36(re).py
+++ 다이나믹/기출문제/Q_36(re).py
@@ -1,25 +1,49 @@
-def edit_dist(str1, str2):
-    n = len(str1)
-    m = len(str2)
+def edit_text(wordA, wordB):
+    n = len(wordA)
+    m = len(wordB)
 
     dp = [[0] * (m + 1) for _ in range(n + 1)]
-
+    for i in range(1, m + 1):
+        dp[0][i] = i
     for i in range(1, n + 1):
         dp[i][0] = i
-    
-    for j in range(1, m + 1):
-        dp[0][j] = j
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            if str1[i - 1] == str2[j - 1]:
+            if wordA[i - 1] == wordB[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
             else:
-                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
 
     return dp[n][m]
 
-str1 = input()
-str2 = input()
 
-print(edit_dist(str1, str2))
+word_a = input()
+word_b = input()
+print(edit_text(word_a, word_b))
+
+# def edit_dist(str1, str2):
+#     n = len(str1)
+#     m = len(str2)
+
+#     dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+#     for i in range(1, n + 1):
+#         dp[i][0] = i
+
+#     for j in range(1, m + 1):
+#         dp[0][j] = j
+
+#     for i in range(1, n + 1):
+#         for j in range(1, m + 1):
+#             if str1[i - 1] == str2[j - 1]:
+#                 dp[i][j] = dp[i - 1][j - 1]
+#             else:
+#                 dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+
+#     return dp[n][m]
+
+# str1 = input()
+# str2 = input()
+
+# print(edit_dist(str1, str2))
--- 최단경로/기출문제/Q_38(re).py
+++ 최단경로/기출문제/Q_38(re).py
@@ -1,31 +1,62 @@
-INF = int(1e9)
-
+# 플로이드 워셜 수행
+INF = 1e9
 n, m = map(int, input().split())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
+arr = [[INF] * (n + 1) for _ in range(n + 1)]
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i == j:
-            graph[i][j] = 0
+            arr[i][j] = 1
 
-for _ in range(m):
+for i in range(m):
     a, b = map(int, input().split())
-    graph[a][b] = 1
+    arr[a][b] = 1
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        for k in range(1, n + 1):
-            graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            arr[a][b] = min(arr[a][b], arr[a][k] + arr[k][b])
 
 result = 0
-print(graph)
 for i in range(1, n + 1):
     count = 0
     for j in range(1, n + 1):
-        if graph[i][j] != INF or graph[j][i] != INF:
+        if arr[i][j] != INF or arr[j][i] != INF:
             count += 1
-    
+
     if count == n:
         result += 1
 
 print(result)
+
+# INF = int(1e9)
+
+# n, m = map(int, input().split())
+# graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         if i == j:
+#             graph[i][j] = 0
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a][b] = 1
+
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         for k in range(1, n + 1):
+#             graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
+
+# result = 0
+# print(graph)
+# for i in range(1, n + 1):
+#     count = 0
+#     for j in range(1, n + 1):
+#         if graph[i][j] != INF or graph[j][i] != INF:
+#             count += 1
+
+#     if count == n:
+#         result += 1
+
+# print(result)
